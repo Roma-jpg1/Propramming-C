7de2804a852017497d8d4f731dd9201ade395c53
Roma-jpg1 <rpetrov2006@mail.ru>
2024-10-10 15:10:28 +0700

    Add files via upload

---
+++ lab44.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+int k=1;
+int i;
+int n;
+scanf("%d", &n);
+for(i=0; i<n; i++){
+	for(int j=0;j<n;j++){
+		printf("%d ", k);
+		k+=1;}
+	printf("\n");
+}
+
+
+
+
+
+
+
+}
