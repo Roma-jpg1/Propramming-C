d89d7a03c2c9d9592ea66a18c6039b3d52156802
Roma-jpg1 <adduser@MSI>
2025-03-04 14:22:46 +0700

    check1

---
+++ sort.c
@@ -0,0 +1,24 @@
+void Insertsort(int n, int a[], int *Cf, int *Mf) {
+    for (int i = 1; i < n; i++) {
+        int t = a[i];
+        (*Mf)++;
+        int j = i - 1;
+
+        while (j >= 0) {
+            (*Cf)++;  
+            if (t < a[j]) {
+                a[j + 1] = a[j];  
+                (*Mf)++;
+                j = j - 1;
+            } else {
+                break;
+            }
+        }
+
+        a[j + 1] = t;  
+        (*Mf)++;
+    }
+} //проверка
+
+
+//проверка3
