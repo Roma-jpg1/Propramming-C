dc1285b677716cd4d3cc20c1416ba27dc755d3dd
Roma-jpg1 <rpetrov2006@mail.ru>
2025-05-22 20:53:01 +0700

    Delete funcchisla.h

--- funcchisla.h
+++
@@ -1,8 +0,0 @@
-#include <stdbool.h>
-#include <stdio.h>
-
-bool is_special_date(int day, int month, int year);
-
-void process_years(int start_year, int end_year, FILE *file);
-
-bool is_leap_year(int year);
